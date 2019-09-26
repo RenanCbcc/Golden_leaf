fadaa11c010c2884b5de03d793f60415e6757d80
renan rosa <renannojosa@gmail.com>
2019-09-25 21:05:08 -0300

    User Authentication with Flask-HTTPAuth implemented.

---
+++ app/api/clerk.py
@@ -0,0 +1,39 @@
+from flask import jsonify, request, url_for, g
+from flask_httpauth import HTTPBasicAuth
+from app.models.tables import Clerk
+from app.api.erros import unauthorized, forbidden
+from app.api import api
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(email_or_token, password):
+    if password == '':
+        g.current_user = Clerk.verify_auth_token(email_or_token)
+        g.token_used = True
+        return g.current_user is not None
+    clerk = Clerk.query.filter_by(email=email_or_token).first()
+    if not clerk:
+        return False
+    g.current_user = clerk
+    return Clerk.verify_password(password)
+
+
+@auth.error_handler
+def auth_error():
+    return unauthorized()
+
+
+@api.before_request
+@auth.login_required
+def before_request():
+    if not g.current_user.confirmed:
+        return forbidden()
+
+
+@api.route('/token')
+def get_token():
+    if g.token_used:
+        return unauthorized()
+    return jsonify({'token': g.current_user.generate_auth_token(expiration=3600), 'expiration': 3600})
--- app/api/erros.py
+++ app/api/erros.py
@@ -12,28 +12,35 @@ def page_not_found(error):
 
 
 @api.app_errorhandler(ValidationError)
-def internal_server_error(error):
-    response = response = jsonify({'error': 'Internal server error'})
+def internal_server_error(message):
+    response = jsonify({'error': 'Internal server error', 'message': message})
     response.status_code = 500
     return response
 
 
 @api.errorhandler(400)
-def validation_error(error):
-    response = jsonify({'error': 'Bad request'})
+def validation_error(message):
+    response = jsonify({'error': 'Bad request', 'message': message})
     response.status_code = 400
     return response
 
 
+@api.errorhandler(401)
+def unauthorized(message):
+    response = jsonify({'error': 'Invalid credentials', 'message': message})
+    response.status_code = 401
+    return response
+
+
 @api.errorhandler(403)
-def forbidden(error):
-    response = jsonify({'error': 'Forbidden'})
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
     response.status_code = 403
     return response
 
 
 @api.errorhandler(405)
-def method_not_allowed(error):
-    response = jsonify({'error': 'Method not allowed'})
+def method_not_allowed(message):
+    response = jsonify({'error': 'Method not allowed', 'message': message})
     response.status_code = 405
     return response
