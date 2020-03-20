8a8652291b83f9e74cac76a050e0f59e00d8db69
Renan Rosa <renannojosa@gmail.com>
2020-03-20 19:42:44 -0300

    Secret key set.

--- app/settings.py
+++ app/settings.py
@@ -15,7 +15,8 @@ class BaseConfig:
     JSON_AS_ASCII = False
 
 
-class ProductionConfig(BaseConfig):    
+class ProductionConfig(BaseConfig):
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
